6989dca5f7177ccce08df89178318e2df4deae5f
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2014-06-17 14:03:33 +0100

    Initial revision

---
+++ cutout/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  cutout
+//
+//  Created by Eduardo on 17/06/2014.
+//  Copyright (c) 2014 IoA. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
